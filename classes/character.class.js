d0e87835890e7df91c6002f0eeba0404d93a0e6c
Lukas Strnad <lukas.strnad222@gmail.com>
2025-03-16 18:35:59 +0100

    added invisible wall

--- classes/character.class.js
+++ classes/character.class.js
@@ -25,11 +25,11 @@ class Character extends MovableObject {
                 this.x += this.speed;
                 this.otherDirection = false;
             }
-            if (this.world.keyboard.LEFT) {
+            if (this.world.keyboard.LEFT && this.x > 0) {
                 this.x -= this.speed;
                 this.otherDirection = true;
             }    
-            this.world.camera_x = -this.x +20.5;        
+            this.world.camera_x = -this.x + 100;        
         }, 1000 / 60);
 
         setInterval(() => {
